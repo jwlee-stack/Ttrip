af56dd9fe3761d05598839d1df7f2a6de9f291c8
HongMinKi <gaogai2@naver.com>
2023-04-24 14:56:33 +0900

    [FEAT] 온보딩 레이아웃 작성(S08P31D104-192)

---
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/OnBoardingBannerFragment.kt
@@ -0,0 +1,39 @@
+package org.sfy.ttrip.presentation.init
+
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
+import org.sfy.ttrip.R
+import org.sfy.ttrip.databinding.FragmentOnboardingBannerBinding
+import org.sfy.ttrip.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class OnBoardingBannerFragment :
+    BaseFragment<FragmentOnboardingBannerBinding>(R.layout.fragment_onboarding_banner) {
+
+    private var bannerPosition = -1
+
+    override fun initView() {
+
+        val bannerData = listOf(
+            R.drawable.ic_onboarding_puzzles,
+            R.drawable.ic_onboarding_task,
+            R.drawable.ic_onboarding_travel
+        )
+
+        bannerPosition = arguments?.getInt("banner_position")!!
+        with(bannerData[bannerPosition]) {
+            binding.ivOnboardingBanner.setImageResource(bannerData[bannerPosition])
+            when (bannerPosition) {
+                0 -> {
+                    binding.apply {
+                        tvOnboardingPuzzles1.visibility = View.VISIBLE
+                        llOnboardingPuzzles2.visibility = View.VISIBLE
+                    }
+                }
+                1 -> {}
+                2 -> {}
+                else -> {}
+            }
+        }
+    }
+}
