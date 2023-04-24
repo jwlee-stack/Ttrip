d1dab566a7fbf067c392a45ec3058afb1b00fe90
윤세림 <tpfla9953@naver.com>
2023-04-24 15:56:14 +0900

    [FEAT] Google Map API 연결 (#S08P31D104-72)

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
@@ -1,14 +1,25 @@
 package org.sfy.ttrip.presentation.live
 
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.sfy.ttrip.R
 import org.sfy.ttrip.databinding.FragmentBoardBinding
 import org.sfy.ttrip.presentation.base.BaseFragment
 
 @AndroidEntryPoint
-class LiveFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_live) {
+class LiveFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_live), OnMapReadyCallback {
+
+    private var map: GoogleMap? = null
 
     override fun initView() {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
