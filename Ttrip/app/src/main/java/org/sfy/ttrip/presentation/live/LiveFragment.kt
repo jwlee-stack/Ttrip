90d1d2a6bb17fc1dc0e173589efdda4951ea9bd8
윤세림 <tpfla9953@naver.com>
2023-04-25 17:30:35 +0900

    [FEAT] 화면에 보이는 범위에 대한 위경도 정보 받아오기 (#S08P31D104-184)

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
@@ -3,15 +3,20 @@ package org.sfy.ttrip.presentation.live
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.VisibleRegion
 import dagger.hilt.android.AndroidEntryPoint
 import org.sfy.ttrip.R
 import org.sfy.ttrip.databinding.FragmentBoardBinding
 import org.sfy.ttrip.presentation.base.BaseFragment
 
 @AndroidEntryPoint
-class LiveFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_live), OnMapReadyCallback {
+class LiveFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_live), OnMapReadyCallback,
+    GoogleMap.OnCameraMoveListener {
 
-    private var map: GoogleMap? = null
+    private lateinit var map: GoogleMap
+    private lateinit var visibleRegion: VisibleRegion
 
     override fun initView() {
         val mapFragment =
@@ -21,5 +26,14 @@ class LiveFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_live),
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.setOnCameraMoveListener(this)
+    }
+
+    override fun onCameraMove() {
+        visibleRegion = map.projection.visibleRegion
+        val latLngBounds: LatLngBounds = visibleRegion.latLngBounds
+        val northeast: LatLng = latLngBounds.northeast
+        val southwest: LatLng = latLngBounds.southwest
+        // API 호출에 필요한 작업 수행
     }
 }
