879ac46c29732102139dfdfd4177a27443732d85
hong0708 <gaogai2@naver.com>
2023-04-27 01:25:42 +0900

    [CHORE] 로그인 화면 이동 임시 수정

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/LoginFragment.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/LoginFragment.kt
@@ -1,6 +1,8 @@
 package org.sfy.ttrip.presentation.init
 
+import android.content.Intent
 import dagger.hilt.android.AndroidEntryPoint
+import org.sfy.ttrip.MainActivity
 import org.sfy.ttrip.R
 import org.sfy.ttrip.databinding.FragmentLoginBinding
 import org.sfy.ttrip.presentation.base.BaseFragment
@@ -19,7 +21,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
             }
 
             tvLogin.setOnClickListener {
-                navigate(LoginFragmentDirections.actionLoginFragmentToSignUpInformationFragment())
+                // 추후 로그인 이후 회원 정보 입력 api 연결 시 수정 예정
+                //navigate(LoginFragmentDirections.actionLoginFragmentToSignUpInformationFragment())
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
             }
         }
     }
