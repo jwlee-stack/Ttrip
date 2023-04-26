9c4f44f9ba2a0bcab76bf1a8ac1563a08dd9bc0e
윤세림 <tpfla9953@naver.com>
2023-04-26 13:07:37 +0900

    [FEAT] 현재 위치 트래킹과 지도 이동시 현재 위치로 이동하는 버튼 추가 (#S08P31D104-181)

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
@@ -1,5 +1,13 @@
 package org.sfy.ttrip.presentation.live
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -8,32 +16,129 @@ import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.VisibleRegion
 import dagger.hilt.android.AndroidEntryPoint
 import org.sfy.ttrip.R
-import org.sfy.ttrip.databinding.FragmentBoardBinding
+import org.sfy.ttrip.databinding.FragmentLiveBinding
 import org.sfy.ttrip.presentation.base.BaseFragment
 
 @AndroidEntryPoint
-class LiveFragment : BaseFragment<FragmentBoardBinding>(R.layout.fragment_live), OnMapReadyCallback,
+class LiveFragment : BaseFragment<FragmentLiveBinding>(R.layout.fragment_live), OnMapReadyCallback,
     GoogleMap.OnCameraMoveListener {
 
     private lateinit var map: GoogleMap
     private lateinit var visibleRegion: VisibleRegion
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val locationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // 권한이 부여된 경우 현재 위치 가져오기
+            getDeviceLocation()
+        } else {
+            // 권한이 거부된 경우 처리
+            showToast("위치 권한이 필요합니다.")
+        }
+    }
 
     override fun initView() {
-        val mapFragment =
-            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        setMapView()
+        initListener()
+        requestLocationPermission()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         map.setOnCameraMoveListener(this)
+        // My Location 레이어 활성화
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        } else {
+            map.apply {
+                isMyLocationEnabled = true
+                uiSettings.isMyLocationButtonEnabled = false
+                uiSettings.isCompassEnabled = false
+            }
+        }
     }
 
     override fun onCameraMove() {
-        visibleRegion = map.projection.visibleRegion
-        val latLngBounds: LatLngBounds = visibleRegion.latLngBounds
-        val northeast: LatLng = latLngBounds.northeast
-        val southwest: LatLng = latLngBounds.southwest
-        // API 호출에 필요한 작업 수행
+        if(::map.isInitialized) {
+            visibleRegion = map.projection.visibleRegion
+            val latLngBounds: LatLngBounds = visibleRegion.latLngBounds
+            val northeast: LatLng = latLngBounds.northeast
+            val southwest: LatLng = latLngBounds.southwest
+            // API 호출에 필요한 작업 수행
+        }
+    }
+
+    private fun initListener() {
+        binding.ivCurrentLocation.setOnClickListener {
+            getDeviceLocation()
+        }
+    }
+
+    private fun setMapView() {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    // 현재 위치 가져오기
+    private fun getDeviceLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        try {
+            // 위치 요청 설정
+            val locationRequest = LocationRequest.create().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+            // 위치 요청 시작
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult) {
+                        // 위치 결과를 가져옴
+                        val location = locationResult.lastLocation
+                        if (location != null) {
+                            // 위치가 null이 아니면 지도 이동
+                            moveCamera(LatLng(location.latitude, location.longitude))
+                            // 위치 정보 수신 중지
+                            fusedLocationClient.removeLocationUpdates(this)
+                        }
+                    }
+                },
+                null
+            )
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message ?: "")
+        }
+    }
+
+    // 지도 이동
+    private fun moveCamera(latLng: LatLng) {
+        if(::map.isInitialized) {
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        }
+    }
+
+    // 위치 권한 확인
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한이 부여되어 있지 않은 경우 권한 요청
+            locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            // 권한이 이미 부여되어 있는 경우 처리할 로직
+            getDeviceLocation()
+        }
     }
 }
