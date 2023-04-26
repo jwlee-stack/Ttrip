c0c4759c6100d2a101cbee450047e791e95d558f
이재완 <jwlee2750@naver.com>
2023-04-26 14:16:00 +0900

    [FEAT] 회원가입, 로그인, 로그아웃 추가(#S08P31D104-253) (#S08P31D104-254) (#S08P31D104-255)

--- Server/api-server/src/main/java/com/ttrip/api/controller/MemberController.java
+++ Server/api-server/src/main/java/com/ttrip/api/controller/MemberController.java
@@ -1,15 +1,17 @@
 package com.ttrip.api.controller;
 
 import com.ttrip.api.dto.DataResDto;
+import com.ttrip.api.dto.memberDto.memberReqDto.MemberLoginReqDto;
+import com.ttrip.api.dto.memberDto.memberReqDto.MemberSignupReqDto;
 import com.ttrip.api.service.MemberService;
+import com.ttrip.api.service.impl.MemberDetails;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "유저 관련 API")
 @RestController
@@ -29,4 +31,23 @@ public class MemberController {
 
         return memberService.findMemberById(1);
     }
+
+    @PostMapping
+    public DataResDto<?> signup(@RequestBody MemberSignupReqDto memberSignupReqDto)
+    {
+        return memberService.signup(memberSignupReqDto);
+    }
+
+    @PostMapping("/login")
+    public DataResDto<?> login(@RequestBody MemberLoginReqDto memberLoginReqDto)
+    {
+        return memberService.login(memberLoginReqDto);
+    }
+
+    @GetMapping("/logout")
+    public DataResDto<?> logout(@AuthenticationPrincipal MemberDetails memberDetails)
+    {
+        return memberService.logout(memberDetails);
+    }
+
 }
