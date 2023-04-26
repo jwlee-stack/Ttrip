bf2330dbaaf0b2e1b73af84c7115c0a4634c8d08
boham97 <kangbosung97@gmail.com>
2023-04-26 17:25:33 +0900

    [CHORE] 오티수정 및 실수 수정(S08P31D104-247)

--- Server/core/src/main/java/com/ttrip/core/utils/ErrorMessageEnum.java
+++ Server/core/src/main/java/com/ttrip/core/utils/ErrorMessageEnum.java
@@ -9,7 +9,8 @@ public enum ErrorMessageEnum {
     ARTICLE_NOT_EXIST("존재하지 않는 게시글입니다"),
     UNEXPECT_VALUE("잘못된 값입니다"),
     NO_AUTH("권한이 없습니다"),
-    SEVER_ERROR("원인을 알수없습니다"),
+    SERVER_ERROR("원인을 알 수 없습니다"),
+    ARTICLE_NOT_EXIST("존재하지 않는 게시글입니다"),
     EXISTS_ACCOUNT("이미 가입한 유저입니다."),
     FAIL_TO_LOGOUT("리프래시 토큰 삭제 실패"),
     FAIL_TO_SIGNUP("회원가입 실패");
