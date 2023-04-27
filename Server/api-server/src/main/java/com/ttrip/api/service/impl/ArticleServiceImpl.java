e39a99714d642c53ceabc7250c5db05199f23996
이재완 <jwlee2750@naver.com>
2023-04-27 09:56:06 +0900

    [FIX] import ErrorMessageEnum

--- Server/api-server/src/main/java/com/ttrip/api/service/impl/ArticleServiceImpl.java
+++ Server/api-server/src/main/java/com/ttrip/api/service/impl/ArticleServiceImpl.java
@@ -11,6 +11,7 @@ import com.ttrip.core.entity.member.Member;
 import com.ttrip.core.repository.applyArticle.ApplyArticleRepository;
 import com.ttrip.core.repository.article.ArticleRepository;
 import com.ttrip.core.repository.member.MemberRepository;
+import com.ttrip.core.utils.ErrorMessageEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
