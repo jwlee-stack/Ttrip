fafc31f2a3f4d7823607c3f2a75ff8e44f43001c
Hong min ki <gaogai2@naver.com>
2023-04-29 01:49:01 +0900

    [FEAT] 유저 정보 입력 플로우 개발(#S08P31D104-105)

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/UserInfoViewModel.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/UserInfoViewModel.kt
@@ -1,18 +1,48 @@
 package org.sfy.ttrip.presentation.init
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class UserInfoViewModel @Inject constructor() : ViewModel() {
 
-    private val _nickNameValid: MutableLiveData<Boolean?> = MutableLiveData()
-    val nickNameValid: LiveData<Boolean?> = _nickNameValid
+    private val _nickNameValid: MutableLiveData<Boolean> = MutableLiveData(false)
+    val nickNameValid: LiveData<Boolean> = _nickNameValid
+
+    private val _userBirthDay: MutableLiveData<Boolean?> = MutableLiveData()
+    val userBirthDay: LiveData<Boolean?> = _userBirthDay
+
+    private val _userSex: MutableLiveData<Boolean?> = MutableLiveData()
+    val userSex: LiveData<Boolean?> = _userSex
+
+    private val _userIntro: MutableLiveData<Boolean?> = MutableLiveData()
+    val userIntro: LiveData<Boolean?> = _userIntro
+
+    private val _profileImgUri: MutableLiveData<Uri?> = MutableLiveData(null)
+    val profileImgUri: MutableLiveData<Uri?> = _profileImgUri
+
+    private var profileImgMultiPart: MultipartBody.Part? = null
 
     fun checkNickName() {
-        _nickNameValid.value = true
+        _nickNameValid.value = !_nickNameValid.value!!
+    }
+
+    fun setProfileImg(uri: Uri, file: File) {
+        viewModelScope.launch {
+            _profileImgUri.value = uri
+            val requestFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            profileImgMultiPart =
+                MultipartBody.Part.createFormData("profileImg", file.name, requestFile)
+        }
     }
 }
