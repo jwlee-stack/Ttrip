01fe442c44f0819aa8c3c2060b2363d6f182f2ac
boham97 <kangbosung97@gmail.com>
2023-04-28 13:38:19 +0900

    [CHORE] swagger 작성 (#S08P31D104-247)

--- Server/api-server/src/main/java/com/ttrip/api/dto/DetailResDto.java
+++ Server/api-server/src/main/java/com/ttrip/api/dto/DetailResDto.java
@@ -1,5 +1,7 @@
 package com.ttrip.api.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,17 +12,29 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "게시글 상세 조화결과", description = "게시글 정보와 모집여부, 내작성글인지 정보가 있다")
 public class DetailResDto {
+        @ApiModelProperty(value = "게시글 아이디", example = "3")
         private Integer articleId;
+        @ApiModelProperty(value = "게시글 작성자", example = "김싸피")
         private String authorName;
+        @ApiModelProperty(value = "게시글 제목", example = "방콕 갈사람")
         private String title;
+        @ApiModelProperty(value = "게시글 내용", example = "제곧내")
         private String content;
+        @ApiModelProperty(value = "국가", example = "태국")
         private String nation;
+        @ApiModelProperty(value = "도시", example = "방콕")
         private String city;
+        @ApiModelProperty(value = "여행 시작 예정일", example = "2023-04-24 17:15:30")
         private LocalDateTime startDate;
+        @ApiModelProperty(value = "여행 막마 예정일", example = "2023-04-24 17:15:30")
         private LocalDateTime endDate;
+        @ApiModelProperty(value = "글 등록시간", example = "2023-04-24 17:15:30")
         private LocalDateTime createdAt;
+        @ApiModelProperty(value = "T: 모집중 F:모집 마감", example = "T")
         private char status;
+        @ApiModelProperty(value = "내 작성글이면 true인 boolean", example = "true")
         private Boolean isMine;
 
 }
--- Server/api-server/src/main/java/com/ttrip/api/dto/SearchResDto.java
+++ Server/api-server/src/main/java/com/ttrip/api/dto/SearchResDto.java
@@ -1,5 +1,7 @@
 package com.ttrip.api.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,16 +12,27 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "SearchResDto", description = "검색 결과 게시글정보와 작성자 정보")
 public class SearchResDto {
+    @ApiModelProperty(value = "게시글 아이디", example = "1")
     private Integer articleId;
+    @ApiModelProperty(value = "작성자 닉네임", example = "김트립")
     private String authorName;
+    @ApiModelProperty(value = "게시글 제목", example = "방콕 여행가시분 있나요?")
     private String title;
+    @ApiModelProperty(value = "게시글 내용", example = "안녕하세요 이번주말에 방콕여행을 가는데 같이...")
     private String content;
+    @ApiModelProperty(value = "나라", example = "태국")
     private String nation;
+    @ApiModelProperty(value = "도시", example = "방콕")
     private String city;
+    @ApiModelProperty(value = "여행 시작 시간", example = "2023-04T27:14:14:25")
     private LocalDateTime startDate;
+    @ApiModelProperty(value = "여행 마감 시간", example = "2023-04T27:14:14:25")
     private LocalDateTime endDate;
+    @ApiModelProperty(value = "글 작성 시간", example = "2023-04T27:14:14:25")
     private LocalDateTime createdAt;
+    @ApiModelProperty(value = "디폴트:T, 마감시: F ", example = "T")
     private char status;
 
 }
