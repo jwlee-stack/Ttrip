9deb75441e626ccf36bc638f2abfcc6b7223d412
윤세림 <tpfla9953@naver.com>
2023-04-28 14:30:09 +0900

    [FEAT] 비밀번호 유효성 확인 및 일치 확인 (#S08P31D104-107)

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/SignUpFragment.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/init/SignUpFragment.kt
@@ -1,6 +1,9 @@
 package org.sfy.ttrip.presentation.init
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
@@ -11,15 +14,38 @@ import org.sfy.ttrip.R
 import org.sfy.ttrip.databinding.FragmentSignUpBinding
 import org.sfy.ttrip.presentation.base.BaseFragment
 import java.util.concurrent.TimeUnit
+import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
 
+    private val singUpViewModel by viewModels<SignUpViewModel>()
     private val mAuth = FirebaseAuth.getInstance()
     private var verificationCode = ""
 
     override fun initView() {
         initListener()
+        passwordMatchCheck()
+        observeData()
+        passwordValidCheck()
+    }
+
+    private fun observeData() {
+        singUpViewModel.passwordMatch.observe(viewLifecycleOwner) { passwd ->
+            singUpViewModel.verifyPhoneSuccess.observe(viewLifecycleOwner) { phone ->
+                if (passwd && phone) {
+                    binding.tvSignUp.apply {
+                        setBackgroundResource(R.drawable.bg_rect_pear_radius10)
+                        isEnabled = true
+                    }
+                } else {
+                    binding.tvSignUp.apply {
+                        setBackgroundResource(R.drawable.bg_rect_whisper_radius10)
+                        isEnabled = false
+                    }
+                }
+            }
+        }
     }
 
     private fun initListener() {
@@ -31,11 +57,13 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sig
                 sendCertificationCode()
             }
             tvVerifyCertificationNumber.setOnClickListener {
-                val credential = PhoneAuthProvider.getCredential(
-                    verificationCode,
-                    etSignUpPhoneCheck.text.toString()
-                )
-                signInWithPhoneAuthCredential(credential)
+                if (etSignUpPhoneCheck.text.toString().isNotEmpty()) {
+                    val credential = PhoneAuthProvider.getCredential(
+                        verificationCode,
+                        etSignUpPhoneCheck.text.toString()
+                    )
+                    signInWithPhoneAuthCredential(credential)
+                }
             }
             tvVerificationAgain.setOnClickListener {
                 binding.apply {
@@ -86,10 +114,65 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sig
                         tvVerifyCertificationNumber.isEnabled = false
                         etSignUpPhoneCheck.isEnabled = false
                         tvVerificationAgain.visibility = View.GONE
+                        singUpViewModel.verifyPhoneSuccess.value = true
                     }
                 } else {
                     showToast("인증 실패했습니다. 다시 시도해주세요.")
                 }
             }
+    }
+
+    private fun passwordMatchCheck() {
+        binding.apply {
+            etSignUpPasswordCheck.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (etSignUpPassword.text.toString() == etSignUpPasswordCheck.text.toString()) {
+                        tvNotMatchPassword.visibility = View.GONE
+                        singUpViewModel.passwordMatch.value = true
+                    } else {
+                        tvNotMatchPassword.visibility = View.VISIBLE
+                        singUpViewModel.passwordMatch.value = false
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (etSignUpPassword.text.toString() == etSignUpPasswordCheck.text.toString()) {
+                        tvNotMatchPassword.visibility = View.GONE
+                        singUpViewModel.passwordMatch.value = true
+                    } else {
+                        tvNotMatchPassword.visibility = View.VISIBLE
+                        singUpViewModel.passwordMatch.value = false
+                    }
+                }
+            })
+        }
+    }
+
+    private fun passwordValidCheck() {
+        binding.etSignUpPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (!Pattern.matches(
+                        "^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$",
+                        binding.etSignUpPassword.text.toString()
+                    )
+                ) {
+                    showToast("8자 이상의 특수문자와 영문 조합의 \n비밀번호를 생성해주세요.")
+                    binding.etSignUpPasswordCheck.isEnabled = false
+                    return
+                } else {
+                    binding.etSignUpPasswordCheck.isEnabled = true
+                }
+            }
+        })
     }
 }
