7467d3b56b6b9398cae1a7e15bea62dd7dd93301
이재완 <jwlee2750@naver.com>
2023-04-29 23:29:06 +0900

    [FEAT] 토큰 재발급/여행 취향 저장API 구현, 이미지 저장 로직 수정(#S08P31D104-258,259)

--- Server/core/src/main/java/com/ttrip/core/entity/survey/Survey.java
+++ Server/core/src/main/java/com/ttrip/core/entity/survey/Survey.java
@@ -1,2 +1,35 @@
-package com.ttrip.core.entity.survey;public class Survey {
+package com.ttrip.core.entity.survey;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.ttrip.core.entity.BaseEntity;
+import com.ttrip.core.entity.member.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Survey extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id",nullable = false)
+    private int surveyId;
+    private int preferNatureThanCity;
+    private int preferPlan;
+    private int preferDirectFlight;
+    private int preferCheapHotelThanComfort;
+    private int preferGoodFood;
+    private int preferCheapTraffic;
+    private int preferPersonalBudget;
+    private int preferTightSchedule;
+    private int preferShoppingThanTour;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    @JsonBackReference
+    private Member member;
 }
--- Server/core/src/main/java/com/ttrip/core/repository/survey/SurveyRepository.java
+++ Server/core/src/main/java/com/ttrip/core/repository/survey/SurveyRepository.java
@@ -1,2 +1,12 @@
-package com.ttrip.core.repository.survey;public interface SurveyRepository {
+package com.ttrip.core.repository.survey;
+
+import com.ttrip.core.entity.survey.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey,Integer> {
+    Optional<Survey> findBySurveyId(Integer memberId);
 }
