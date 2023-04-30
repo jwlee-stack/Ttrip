583de90a3071c083a939421777659a05182013cb
Hong min ki <gaogai2@naver.com>
2023-05-01 07:55:47 +0900

    [FEAT] 회원정보 입력 api 추가(#S08P31D104-286)

--- Ttrip/app/src/main/java/org/sfy/ttrip/data/remote/repository/UserRepositoryImpl.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/data/remote/repository/UserRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package org.sfy.ttrip.data.remote.repository
 
-class UserRepositoryImpl {
+import okhttp3.MultipartBody
+import org.sfy.ttrip.common.util.wrapToResource
+import org.sfy.ttrip.data.remote.datasorce.user.UserRemoteDataSource
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userRemoteDataSource: UserRemoteDataSource
+) : UserRemoteDataSource {
+
+    override suspend fun patchUserInfo(
+        nickName: String,
+        intro: String,
+        gender: String,
+        profileFile: MultipartBody.Part?,
+        markerFile: MultipartBody.Part?,
+        age: String,
+        fcmToken: String
+    ) {
+        wrapToResource {
+            userRemoteDataSource.patchUserInfo(
+                nickName,
+                intro,
+                gender,
+                profileFile,
+                markerFile,
+                age,
+                fcmToken
+            )
+        }
+    }
 }
--- Ttrip/app/src/main/java/org/sfy/ttrip/di/ServiceModule.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/di/ServiceModule.kt
@@ -8,6 +8,7 @@ import org.sfy.ttrip.AuthInterceptorClient
 import org.sfy.ttrip.NoAuthInterceptorClient
 import org.sfy.ttrip.data.remote.service.AuthApiService
 import org.sfy.ttrip.data.remote.service.LiveApiService
+import org.sfy.ttrip.data.remote.service.UserApiService
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -28,4 +29,11 @@ object ServiceModule {
         @AuthInterceptorClient retrofit: Retrofit
     ): LiveApiService =
         retrofit.create(LiveApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideUserApiService(
+        @AuthInterceptorClient retrofit: Retrofit
+    ): UserApiService =
+        retrofit.create(UserApiService::class.java)
 }
