7cf85117b7d14c32d0b605b7981b8f4abf7c7a17
이재완 <jwlee2750@naver.com>
2023-05-01 14:17:14 +0900

    [FEAT] mypage의 컨트롤러/서비스 설계& 내 게시글 조회 구현(#S08P31D104-270)

--- Server/api-server/src/main/java/com/ttrip/api/controller/MypageController.java
+++ Server/api-server/src/main/java/com/ttrip/api/controller/MypageController.java
@@ -1,2 +1,33 @@
-package com.ttrip.api.controller;public class MypageController {
+package com.ttrip.api.controller;
+
+import com.ttrip.api.dto.DataResDto;
+import com.ttrip.api.service.MypageService;
+import com.ttrip.api.service.impl.MemberDetails;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "마이페이지 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mypage")
+public class MypageController {
+    private final MypageService mypageService;
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "내 게시글 조회 성공"),
+            @ApiResponse(code = 400, message = "내 게시글 조회 실패")
+    })
+    @ApiOperation(value = "내 게시글 조회")
+    @GetMapping("/view/articles")
+    public DataResDto<?> viewMyArticles(@AuthenticationPrincipal MemberDetails memberDetails)
+    {
+        return mypageService.viewMyArticles(memberDetails);
+    }
 }
--- Server/api-server/src/main/java/com/ttrip/api/service/impl/MypageServiceImpl.java
+++ Server/api-server/src/main/java/com/ttrip/api/service/impl/MypageServiceImpl.java
@@ -1,2 +1,47 @@
-package com.ttrip.api.service.impl;public class MypageServiceImpl {
+package com.ttrip.api.service.impl;
+
+import com.ttrip.api.dto.DataResDto;
+import com.ttrip.api.dto.SearchResDto;
+import com.ttrip.api.service.MypageService;
+import com.ttrip.core.entity.article.Article;
+import com.ttrip.core.entity.member.Member;
+import com.ttrip.core.repository.article.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MypageServiceImpl implements MypageService {
+    private final ArticleRepository articleRepository;
+
+    @Override
+    public DataResDto<?> viewMyArticles(MemberDetails memberDetails) {
+        Member member = memberDetails.getMember();
+        List<Article> myArticles = articleRepository.findAllByMember(member);
+        List<SearchResDto> searchResultDtoList = new ArrayList<>();
+
+        //코드 중복으로 노란 밑줄 생기는데, toBuild로 묶으면 어떨까요?
+        for (Article article : myArticles) {
+            SearchResDto searchResultDto = SearchResDto.builder()
+                    .articleId(article.getArticleId())
+                    .authorName(article.getMember().getNickname())
+                    .title(article.getTitle())
+                    .content(article.getContent())
+                    .nation(article.getNation())
+                    .city(article.getCity())
+                    .status(article.getStatus())
+                    .startDate(article.getStartDate())
+                    .endDate(article.getEndDate())
+                    .build();
+
+            searchResultDtoList.add(searchResultDto);
+        }
+        return DataResDto.builder()
+                .message("내 게시글 조회 성공")
+                .data(searchResultDtoList)
+                .build();
+    }
 }
