fcc57341441501e54e9288b72c10d6ef5d9d5acc
이재완 <jwlee2750@naver.com>
2023-05-02 09:42:51 +0900

    [CHORE] 이미지/내 정보 업데이트 관련 로그 추가

--- Server/api-server/src/main/java/com/ttrip/api/service/impl/MypageServiceImpl.java
+++ Server/api-server/src/main/java/com/ttrip/api/service/impl/MypageServiceImpl.java
@@ -16,6 +16,7 @@ import com.ttrip.core.entity.member.Member;
 import com.ttrip.core.repository.article.ArticleRepository;
 import com.ttrip.core.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +25,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class MypageServiceImpl implements MypageService {
@@ -60,6 +62,11 @@ public class MypageServiceImpl implements MypageService {
 
     @Override
     public DataResDto<?> updateMember(InfoUpdateReqDto infoUpdateReqDto, MemberDetails memberDetails) {
+        log.info("닉네임: {}",infoUpdateReqDto.getNickname());
+        log.info("성별: {}",Gender.valueOf(infoUpdateReqDto.getGender()));
+        log.info("나이: {}",infoUpdateReqDto.getAge());
+        log.info("인트로: {}",infoUpdateReqDto.getIntro());
+
         if (infoUpdateReqDto.getNickname().isEmpty() || infoUpdateReqDto.getGender() == null || infoUpdateReqDto.getAge() == null)
             throw new BadRequestException("멤버 필수 정보가 누락됐습니다. (닉네임/성별/나이)");
 
@@ -126,6 +133,11 @@ public class MypageServiceImpl implements MypageService {
         String customImgPath = path + member.getMemberUuid() + ".png"; //변경 이미지 경로
         String defaultImgPath = path + "default.png"; //디폴트 이미지 경로
         File profileImg = new File(customImgPath); //파일 객체 생성
+
+        log.info("공통 경로: {}",path);
+        log.info("기본 이미지 경로: {}",defaultImgPath);
+        log.info("변경 이미지 경로: {}",customImgPath);
+        log.info("이미지 파일: {}",img);
 
         //사용자가 이미지를 설정했는지?
         if (!img.isEmpty()) //이미지 설정함
