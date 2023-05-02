027f7d2222c4d68319b0a17a7743d211ef408ccf
조민지 <dnflwhalswl@naver.com>
2023-05-02 11:03:18 +0900

    [CHORE] import 와일드카드 수정

--- Server/api-server/src/main/java/com/ttrip/api/controller/OpenviduController.java
+++ Server/api-server/src/main/java/com/ttrip/api/controller/OpenviduController.java
@@ -10,10 +10,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "Openvidu 관련 API")
 @RestController
