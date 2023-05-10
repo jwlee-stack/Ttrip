4e02b17359769e2b9be30087233833784911f78e
이재완 <jwlee2750@naver.com>
2023-05-10 13:29:48 +0900

    [FIX] Refresh Token security에서 해제 및 에러 수정 (accessToken이 없으므로, memberdetails 객체를 불러오는 것이 불가능 -> 권한에서 전화번호 추출 후, 멤버를 find하는 로직으로 수정)(#S08P31D104-259)

--- Server/api-server/src/main/java/com/ttrip/api/config/security/SecurityConfig.java
+++ Server/api-server/src/main/java/com/ttrip/api/config/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     private static final String[] AUTH_ARR = {
             "/api/members",
             "/api/members/login",
+            "/api/members/reissue",
             "/ws/live",
             "/api-server/src/main/webapp/WEB-INF/views/live.jsp",
             "/ws/live/**/**",
--- Server/api-server/src/main/java/com/ttrip/api/service/MemberService.java
+++ Server/api-server/src/main/java/com/ttrip/api/service/MemberService.java
@@ -10,13 +10,12 @@ import com.ttrip.api.dto.tokenDto.tokenReqDto.TokenReqDto;
 import com.ttrip.api.service.impl.MemberDetails;
 
 import java.io.IOException;
-import java.util.UUID;
 
 public interface MemberService {
     DataResDto<?> signup(MemberSignupReqDto memberSignupReqDto);
     DataResDto<?> login(MemberLoginReqDto memberLoginReqDto);
     DataResDto<?> logout(MemberDetails memberDetails);
-    DataResDto<?> reissue(TokenReqDto tokenReqDto, UUID uuid);
+    DataResDto<?> reissue(TokenReqDto tokenReqDto);
     DataResDto<?> setInfo (MemberUpdateReqDto memberUpdateReqDto, MemberDetails memberDetails) throws IOException;
     DataResDto<?> checkNickname(String nickname);
     DataResDto<?> updateSurvey(SurveyReqDto surveyReqDto, MemberDetails memberDetails);
