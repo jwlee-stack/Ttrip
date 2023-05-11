54334e596eac746ca3666ee83d179255348cc975
조민지 <dnflwhalswl@naver.com>
2023-05-12 01:17:37 +0900

    [FEAT] 뱃지 발급 시 중복 발급 방지 구현 (#S08P31D104-324)

--- Server/core/src/main/java/com/ttrip/core/repository/landmark/BadgeRepository.java
+++ Server/core/src/main/java/com/ttrip/core/repository/landmark/BadgeRepository.java
@@ -1,10 +1,13 @@
 package com.ttrip.core.repository.landmark;
 
 import com.ttrip.core.entity.badge.Badge;
+import com.ttrip.core.entity.badge.Landmark;
+import com.ttrip.core.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BadgeRepository extends JpaRepository<Badge, Integer> {
     List<Badge> findByMemberMemberId(Integer memberId);
+    boolean existsByMemberAndLandmark(Member member, Landmark landmark);
 }
