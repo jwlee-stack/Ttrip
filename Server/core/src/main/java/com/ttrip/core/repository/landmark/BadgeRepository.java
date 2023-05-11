714777d206d5c8f95762564db908fe2c8819d339
조민지 <dnflwhalswl@naver.com>
2023-05-12 00:47:13 +0900

    [FEAT] 뱃지 조회 api 구현 (#S08P31D104-436)

--- Server/core/src/main/java/com/ttrip/core/repository/landmark/BadgeRepository.java
+++ Server/core/src/main/java/com/ttrip/core/repository/landmark/BadgeRepository.java
@@ -3,6 +3,8 @@ package com.ttrip.core.repository.landmark;
 import com.ttrip.core.entity.badge.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BadgeRepository extends JpaRepository<Badge, Integer> {
+import java.util.List;
 
+public interface BadgeRepository extends JpaRepository<Badge, Integer> {
+    List<Badge> findByMemberMemberId(Integer memberId);
 }
