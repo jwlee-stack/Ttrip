d9888fa81933965671f2e7ecaba3b70516851784
조민지 <dnflwhalswl@naver.com>
2023-05-12 01:23:51 +0900

    [CHORE] 뱃지 조회 repository 수정 (#S08P31D104-436)

--- Server/core/src/main/java/com/ttrip/core/repository/landmark/BadgeRepository.java
+++ Server/core/src/main/java/com/ttrip/core/repository/landmark/BadgeRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BadgeRepository extends JpaRepository<Badge, Integer> {
-    List<Badge> findByMemberMemberId(Integer memberId);
+    List<Badge> findByMember(Member member);
     boolean existsByMemberAndLandmark(Member member, Landmark landmark);
 }
