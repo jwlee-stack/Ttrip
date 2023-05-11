5ff2b6de566dbb63054520913f7304e0098ffb41
홍민기 <gaogai2@naver.com>
2023-05-11 14:47:17 +0900

    [CHORE] 채팅 리스트 뷰 미세 수정 및 라이브 바텀 네비 뷰 여부 추가

--- Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
+++ Ttrip/app/src/main/java/org/sfy/ttrip/presentation/live/LiveFragment.kt
@@ -86,6 +86,7 @@ class LiveFragment : BaseFragment<FragmentLiveBinding>(R.layout.fragment_live), 
         getOpenViduToken()
         showUserProfileDialog()
         initObserve()
+        (activity as MainActivity).hideBottomNavigation(true)
     }
 
     override fun onAttach(context: Context) {
